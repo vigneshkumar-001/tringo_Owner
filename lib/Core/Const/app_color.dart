40e998a2b87255ed1a90367488e19b56b5f5d4fc
vigneshkumar-001 <vignesh@fenizotechnologies.com>
2025-10-27 16:31:14 +0530

    maraa add

--- lib/Core/Const/app_color.dart
+++ lib/Core/Const/app_color.dart
@@ -1,8 +1,13 @@
 import 'dart:ui';
 
 class AppColor {
-     static const scaffoldColor = Color(0XFFFFFFFF);
-     static const mediumGray = Color(0xff777777);
-     static const leftArrow = Color(0XFFF5F5F5);
-     static const mildBlack = Color(0XFF252525);
+  static const scaffoldColor = Color(0XFFFFFFFF);
+  static const mediumGray = Color(0xff777777);
+  static const leftArrow = Color(0XFFF5F5F5);
+  static const mildBlack = Color(0XFF252525);
+  static const lightSkyBlue = Color(0XFFE6F2F9);
+  static const mediumGreen = Color(0XFF31CC64);
+  static const lightGray = Color(0XFFF2F2F2);
+  static const red = Color(0xffF5465C);
+  static const black = Color(0xff000000);
 }
