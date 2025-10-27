1bb1df2d120baf454bcecd2de053123c814921ce
vigneshkumar-001 <vignesh@fenizotechnologies.com>
2025-10-27 13:36:38 +0530

    maara  add

---
+++ lib/Core/Const/app_images.dart
@@ -0,0 +1,11 @@
+class AppImages {
+  static String addImage = 'Assets/Images/Add_Image.png';
+  static String addListImage = 'Assets/Images/Add_List_Image.png';
+  static String addProduct = 'Assets/Images/Add_Product.png';
+  static String downArrow = 'Assets/Images/Down_Arrow.png';
+  static String leftArrow = 'Assets/Images/left_Arrow.png';
+  static String rightStickArrow = 'Assets/Images/Right_Stick_Arrow.png';
+  static String iImage = 'Assets/Images/I_Image.png';
+  static String registerBCImage = 'Assets/Images/Register_BCimage.png';
+  static String shopInfoImage = 'Assets/ImagesShop_Info_Image.png';
+}
