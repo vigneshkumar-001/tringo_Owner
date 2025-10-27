fb214cc76bac7a20e3d1ef814673bcaba5018036
vigneshkumar-001 <vignesh@fenizotechnologies.com>
2025-10-27 13:56:18 +0530

    maara change

--- lib/Core/Const/app_color.dart
+++ lib/Core/Const/app_color.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 class AppColor {
-
      static const scaffoldColor = Color(0XFFFFFFFF);
+     static const whiteSmoke = Color(0xffF5F5F5);
+     static const mediumGray = Color(0xff777777);
 }
