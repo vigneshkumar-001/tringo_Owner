b565b706a1db800ac30eb7f87f5573085cb003f4
appfeni <app@fenizotechnologies.com>
2025-11-11 10:58:25 +0530

    vignesh changes

---
+++ android/app/src/main/java/com/fenizo/tringo_vendor/tringo_vendor/CallReceiver.kt
@@ -0,0 +1,34 @@
+package com.fenizo.tringo_vendor.tringo_vendor
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.telephony.TelephonyManager
+import androidx.core.content.ContextCompat
+
+class CallReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+        android.util.Log.d("TringoReceiver", "PHONE_STATE = $state")
+        when (state) {
+            TelephonyManager.EXTRA_STATE_RINGING -> {
+                androidx.core.content.ContextCompat.startForegroundService(
+                    context,
+                    Intent(context, TringoOverlayService::class.java).apply {
+                        putExtra("cmd", "show")
+                        putExtra("name", "Tringo Incoming")
+                        putExtra("number", "7871222346")
+                    }
+                )
+            }
+            TelephonyManager.EXTRA_STATE_IDLE,
+            TelephonyManager.EXTRA_STATE_OFFHOOK -> {
+                androidx.core.content.ContextCompat.startForegroundService(
+                    context,
+                    Intent(context, TringoOverlayService::class.java).apply { putExtra("cmd", "hide") }
+                )
+            }
+        }
+    }
+
+}
