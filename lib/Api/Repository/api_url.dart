36e85d0046f977ed3d2d17cbfb91afb43f0813f2
appfeni <app@fenizotechnologies.com>
2025-12-23 18:58:35 +0530

    vignesh changes

--- lib/Api/Repository/api_url.dart
+++ lib/Api/Repository/api_url.dart
@@ -1,7 +1,8 @@
 class ApiUrl {
-  static const String stagingBase =
-      "https://fenizo-tringo-backend-12ebb106711d.herokuapp.com/";
-  static const String base = "https://bknd.tringobiz.com//";
+  // static const String stagingBase =
+  //     "https://fenizo-tringo-backend-12ebb106711d.herokuapp.com/";
+
+  static const String base = "https://bknd.tringobiz.com/";
 
   static const String base1 = "https://vk6shsk1-3000.inc1.devtunnels.ms/";
   static const String register = "${base}api/v1/auth/request-otp";
